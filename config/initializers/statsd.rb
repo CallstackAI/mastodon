b83239b7f9f19bbc35bdddcf4559677468cac496
Adam Pavlisin <lostmines.eu@gmail.com>
2024-07-31 14:24:40 +0200

    Update statsd.rb

--- config/initializers/statsd.rb
+++ config/initializers/statsd.rb
@@ -7,7 +7,7 @@ if ENV['STATSD_ADDR'].present?
     statsd = Statsd.new(host, port)
     statsd.namespace = ENV.fetch('STATSD_NAMESPACE') { ['Mastodon', Rails.env].join('.') }
 
-    NSA.inform_statsd(statsd) do |informant|
+    NSA.informant_collect(statsd) do |informant|
       informant.collect(:action_controller, :web)
       informant.collect(:active_record, :db)
       informant.collect(:active_support_cache, :cache)
